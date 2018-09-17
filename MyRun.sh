04157849db6585a7943d95dcbadd1bb183aab15d
rferris18 <43316456+rferris18@users.noreply.github.com>
2018-09-17 18:36:21 +0100

    Update MyRun.sh

--- MyRun.sh
+++ MyRun.sh
@@ -8,9 +8,8 @@ cd /var/www/html/
 rm index.html
 wget https://raw.githubusercontent.com/puppetlabs/exercise-webpage/master/index.html
 cd /etc/nginx/sites-available/
-rm default
-# Write listening port to :8080 remove default file and replace with updated configuration
-wget https://raw.githubusercontent.com/rferris18/jobtest/master/default
+# Write listening port to :8080 using 'sed'
+sed -i 's/80/8080' default
 # Restart nginx to pick up changes
 sudo service nginx restart
 
