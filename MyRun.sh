49aba4c34f156b4d6f9a1e84214b846d5c152cb9
rferris18 <43316456+rferris18@users.noreply.github.com>
2018-09-18 14:17:03 +0100

    Update MyRun.sh

--- MyRun.sh
+++ MyRun.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
+# For subsequent runs of the executable to be successfull the port number with in the conf needs to be overwritten to display port 80
+sed -i 's/8080/80/' /etc/nginx/sites-available/default
 # Install Nginx
-apt-get -y update
-apt-get -y install nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
 # Remove defualt index.html file and import index.html from github
 cd /var/www/html/
-rm index.html
+sudo rm index.html
 wget https://raw.githubusercontent.com/puppetlabs/exercise-webpage/master/index.html
 cd /etc/nginx/sites-available/
-# Write listening port to :8080 using 'sed'
+# Write to conf so that port is listening to :8080. when this executable is run again the port will change back to 80 so that when the below command is run the port number is not changed to 808080
 sed -i 's/80/8080' default
 # Restart nginx to pick up changes
 sudo service nginx restart
